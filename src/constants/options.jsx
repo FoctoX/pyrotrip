17dfb79d8437ce5dc3556c318fade2ba868a252d
Focto <fabiogmg1410@gmail.com>
2024-12-02 13:47:18 +0700

    Initial commit

---
+++ src/constants/options.jsx
@@ -0,0 +1,51 @@
+export const SelectTravelesList=[
+    {
+        id:1,
+        title:'Just Me',
+        desc:'A sole traveles in exploration',
+        icon:'',
+        people:'1 Person'
+    },
+    {
+        id:2,
+        title:'A Couple',
+        desc:'Two traveles in tandem',
+        icon:'',
+        people:'1 Person'
+    },
+    {
+        id:3,
+        title:'Family',
+        desc:'A group of fun loving adventure',
+        icon:'',
+        people:'1 Person'
+    },
+    {
+        id:4,
+        title:'Friends',
+        desc:'A bunch of thrill-seekes',
+        icon:'',
+        people:'1 Person'
+    },
+]
+
+export const SelectBudgetOptions=[
+    {
+        id:1,
+        title:'Cheap',
+        desc:'Stay conscious of costs',
+        icon:''
+    },
+    {
+        id:2,
+        title:'Moderate',
+        desc:'Keep cost on the average side',
+        icon:''
+    },
+    {
+        id:3,
+        title:'Luxury',
+        desc:'Dont worry about cost',
+        icon:''
+    }
+]
